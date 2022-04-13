378622bd499a3c5efe0a7c5549e80e657d59a66d
Xi Gao <xiga6451@colorado.edu>
2022-04-13 17:28:57 -0600

    Removed extra unnecessary files

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
-import { data } from './data';
 import Split from 'react-split';
 import { nanoid } from 'nanoid';
 import 'react-mde/lib/styles/css/react-mde-all.css';
